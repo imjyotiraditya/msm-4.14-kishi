b9e6f00e5d868dac3b16c7fb6eb2054b7074350e
danascape <saalimquadri1@gmail.com>
2021-02-20 03:48:06 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+TC_DIR="$HOME/tc"
+
+export KBUILD_BUILD_HOST="notkernel"
+export KBUILD_BUILD_USER="Panda"
+
+make O=out ARCH=arm64 vendor/kishi-perf_defconfig
+
+PATH="${TC_DIR}/clang/bin:${TC_DIR}/gcc/bin:${TC_DIR}/gcc_32/bin:${PATH}"
+
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang \
+		CLANG_TRIPLE=aarch64-linux-gnu- \
+		CROSS_COMPILE=aarch64-linux-android- \
+		CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+		TARGET_PRODUCT=msmnile \
+		PROJECT_NAME=19696 \
+		OPPO_TARGET_DEVICE=MSM_19696
