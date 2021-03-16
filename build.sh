3184feefa0598d36bce55a5e20761ba89c803d59
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-03-16 12:37:53 +0100

    adapt for fish
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
-TC_DIR="$HOME/tc"
+#!/usr/bin/fish
+
+set TC_DIR "$HOME/tc"
 
 export KBUILD_BUILD_HOST="notkernel"
 export KBUILD_BUILD_USER="Panda"
 
 make O=out ARCH=arm64 vendor/kishi-perf_defconfig
 
-PATH="${TC_DIR}/clang/bin:${TC_DIR}/gcc/bin:${TC_DIR}/gcc_32/bin:${PATH}"
+set PATH "$TC_DIR/clang/bin:$TC_DIR/gcc/bin:$TC_DIR/gcc_32/bin:$PATH"
 
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang \
+make -j(nproc --all) O=out ARCH=arm64 CC=clang \
 		CLANG_TRIPLE=aarch64-linux-gnu- \
 		CROSS_COMPILE=aarch64-linux-android- \
 		CROSS_COMPILE_ARM32=arm-linux-androideabi-
