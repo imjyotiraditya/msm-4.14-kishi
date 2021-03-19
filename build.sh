16510317f8bb4a0c6d2e2fd4d4b3c620d42995c5
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-03-20 02:27:38 +0530

    build.sh: update build script
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- build.sh
+++ build.sh
@@ -1,15 +1,47 @@
 #!/usr/bin/fish
 
-set TC_DIR "$HOME/tc"
-
+set PATH $HOME/tc/proton-clang/bin $PATH
 export KBUILD_BUILD_HOST="notkernel"
 export KBUILD_BUILD_USER="Panda"
 
-make O=out ARCH=arm64 vendor/kishi-perf_defconfig
+# Push kernel to channel
+function push
+    cd AnyKernel3
+    set ZIP (echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id"
+end
 
-set PATH "$TC_DIR/clang/bin:$TC_DIR/gcc/bin:$TC_DIR/gcc_32/bin:$PATH"
+# Fin Error
+function finerr
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+end
 
-make -j(nproc --all) O=out ARCH=arm64 CC=clang \
-		CLANG_TRIPLE=aarch64-linux-gnu- \
-		CROSS_COMPILE=aarch64-linux-android- \
-		CROSS_COMPILE_ARM32=arm-linux-androideabi-
+# Compile plox
+function compile
+    make O=out ARCH=arm64 vendor/kishi-perf_defconfig
+    make -j(nproc --all) O=out ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm \
+        OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
+        CROSS_COMPILE=aarch64-linux-gnu- \
+        CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+        Image.gz-dtb
+
+    if test $status -eq 0
+        cd AnyKernel3
+        make clean
+        cp ../out/arch/arm64/boot/Image.gz-dtb .
+        make notkernel
+        cp NotKernel*.zip ~/upload
+        cd ..
+     else
+        finerr
+     end
+end
+
+compile
+push
